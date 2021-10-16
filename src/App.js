ba7a5f2db25e8d579081c9beb30919dd1c0d1343
MoosaGhias <moosa.ghias@gmail.com>
2021-10-16 01:40:35 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(user => this.setState({monsters: user}))
+      //the .then user fills up teh monsters array with the user which is the data that came from the json
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>Monster Employees</h1>
+        <SearchBox
+          placeholder ='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
