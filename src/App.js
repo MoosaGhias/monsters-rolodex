1e3640693411da04c028ea968f941d0b616864e0
MoosaGhias <moosa.ghias@gmail.com>
2021-10-30 01:33:36 -0700

    check json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {CardList} from './components/card-list/card-list.component';
 import {SearchBox} from './components/search-box/search-box.component';
 
+
+
 class App extends Component{
   constructor(){
     super();
@@ -28,6 +30,7 @@ class App extends Component{
     return (
       <div className="App">
         <h1>Monster Employees</h1>
+
         <SearchBox
           placeholder ='search monsters'
           handleChange={this.handleChange}
