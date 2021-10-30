f0da9536096020d636c8f256ff439b330db05302
MoosaGhias <moosa.ghias@gmail.com>
2021-10-30 01:35:14 -0700

    json

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component{
     return (
       <div className="App">
         <h1>Monster Employees</h1>
+        
 
         <SearchBox
           placeholder ='search monsters'
